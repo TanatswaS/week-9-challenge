b737819933d7492c86c79edee01c7fe1494097a9
Mister Tanatswa <tanatswasingano@gmail.com>
2022-06-03 20:42:52 -0400

    set up iquirer

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+//inquire to generate questions
+inquirer.prompt(
+  [
+    {
+        type: 'input',
+        messages
+    }
+  ]
+)
